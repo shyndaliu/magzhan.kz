54ca5638087b4b54347cde2ec8a4f1136e9ac276
uldana shyndali <122513545+shyndaliu@users.noreply.github.com>
2024-05-22 23:55:30 +0500

    working on header

---
+++ app/not-found.tsx
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <div className="mx-auto flex flex-col h-lvh mt-[-50px]  items-center justify-center">
+        <h1 className="text-8xl">404</h1>
+        <p className="text-xl">Ойбуй... Бұл парақша бізде жоқ екен!</p>
+    </div>
+}
